ef9a3e33c2445949f0fb313ae6f563a23a689bb0
Adam Davis <adamsdavis1976@gmail.com>
2014-09-30 17:07:56 +0100

    First commit of test repo

---
+++ maven-test-project/src/test/java/ITMyMuleTest.java
@@ -0,0 +1,32 @@
+
+import junit.framework.Assert;
+
+import org.junit.Test;
+import org.mule.api.MuleMessage;
+import org.mule.module.client.MuleClient;
+import org.mule.tck.junit4.FunctionalTestCase;
+
+public class ITMyMuleTest extends FunctionalTestCase
+{
+    @Override
+    protected String getConfigResources()
+    {
+      return "maven-test-project.xml";
+    }
+ 
+    @Test
+    public void testValidJob() throws Exception
+    {
+      MuleClient client = new MuleClient(muleContext);
+      MuleMessage result = client.send("http://localhost:8085/test", "", null);
+      
+     
+      //final String value = muleContext.getRegistry().get("http.port");
+      //System.out.println("env port\n " +muleContext.getRegistry().toString());
+      Assert.assertEquals(result.getPayloadAsString().startsWith("Hello World"), true);
+ 
+     
+    }
+    
+}
+ 
